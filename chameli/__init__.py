1c1f83fe4e92e00e33fae144ae880087e738142e
Pankaj <your.email@example.com>
2026-01-30 23:25:36 +0530

    Bump version to 0.1.10

--- chameli/__init__.py
+++ chameli/__init__.py
@@ -6,7 +6,7 @@ from importlib.resources import files
 from logging.handlers import TimedRotatingFileHandler
 from typing import Optional, List
 
-__version__ = "0.1.9"
+__version__ = "0.1.10"
 
 from .config import is_config_loaded, load_config
 
